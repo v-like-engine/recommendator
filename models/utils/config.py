acf8e3f205d1563dfcd48451294ffb6231805718
v-like-engine <cypepv@yandex.ru>
2023-11-28 20:28:23 +0300

    Change utils and config

--- models/utils/config.py
+++ models/utils/config.py
@@ -1,3 +1,4 @@
 names = ['user_id', 'movie_id', 'rating', 'timestamp']
-user_col = "user_id"
-movie_col = 'movie_id'
+user_col = names[0]
+movie_col = names[1]
+ratings_col = names[2]
---
+++ models/utils/utils.py
@@ -0,0 +1,7 @@
+import torch
+import numpy as np
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
